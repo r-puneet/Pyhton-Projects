61ee015a7ea2861ee4688f3f70a89d4759dfb985
Puneet Rajput <68264438+r-puneet@users.noreply.github.com>
2021-03-25 21:55:27 +0530

    Update qrGenerator.py

--- QR Code Generator/qrGenerator.py
+++ QR Code Generator/qrGenerator.py
@@ -4,7 +4,9 @@ qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=20,
                    border=5)
-qr.add_data("https://www.instagram.com/_nishipawar_/")
+print("Enter URL to generate QR CODE:")
+url = input()
+qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color='black', back_color='white')
