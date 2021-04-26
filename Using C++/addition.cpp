3528c442518dcc82680631eaa51e81eaa76043fe
Puneet Rajput <kpuneet90rajput@gmail.com>
2021-04-26 22:40:49 +0530

    commit on master

---
+++ Using C++/addition.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y,sum;
+    cout<<"Enter Number:";
+    cin>>x;
+    cout<<"Enter number:";
+    cin>>y;
+
+    sum=x+y;
+    cout<<"sum:"<<sum;
+}
