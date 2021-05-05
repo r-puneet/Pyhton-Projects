2e05321ec409baf6827756a9c412c74be6b74cc2
Puneet Rajput <kpuneet90rajput@gmail.com>
2021-05-05 23:06:43 +0530

    commit

--- using Python/Enumurate.py
+++ using Python/Enumurate.py
@@ -1,5 +1,8 @@
-# always use double round-brackets while using a list function
-a = list((1,2,6,4,8,5))
-print("Index and its Value:")
+a=[]
+n=int(input("\nEnter Size:"))
+for i in range(0,n):
+    element=int(input("\nEnter Elements:"))
+    a.append(element)
+print("\nIndex and its Value:")
 for index, value in enumerate(a):
     print(index, value)
